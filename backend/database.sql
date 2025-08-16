58b5ee162d7dee85b9ae665c05b4d5770b915779
Vanshika Patel <22it117@charusat.edu.in>
2025-08-17 01:37:35 +0530

    Added companies details

--- backend/database.sql
+++ backend/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE  IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
+    user_id VARCHAR(20) PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'student', 'alumni')),
